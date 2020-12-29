f73747272a9c5c9c7d8f4fe0eded5ab0d561c238
yasin <wuyang@wondersoft.cn>
2020-12-29 10:48:27 +0800

    perf config.go

--- delayqueue/config.go
+++ delayqueue/config.go
@@ -47,10 +47,10 @@ type RedisConfig struct {
 	Password       string
 	MaxIdle        int // 连接池最大空闲连接数
 	MaxActive      int // 连接池最大激活连接数
-	ConnectTimeout int // 连接超时, 单位毫秒
-	ReadTimeout    int // 读取超时, 单位毫秒
-	WriteTimeout   int // 写入超时, 单位毫秒
-	ExpireTime     int //key过期时间，单位秒
+	ConnectTimeout int // 连接超时,单位毫秒
+	ReadTimeout    int // 读取超时,单位毫秒
+	WriteTimeout   int // 写入超时,单位毫秒
+	ExpireTime     int // key过期时间,单位秒
 }
 
 func (this *Config) initConfig(config *Config) {
