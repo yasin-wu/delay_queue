0f1a1baf66ef689b056cbc2e49ce957723bb0783
yasin <wh_wuyang@topsec.com.cn>
2022-06-16 10:55:53 +0800

    feat: 新增自定义logger功能

--- logger/logger.go
+++ logger/logger.go
@@ -15,9 +15,11 @@ type Logger interface {
 type printfLogger struct{}
 
 func (printfLogger) InfoF(format string, args ...interface{}) {
-	fmt.Printf("[INFO] - %s - %s\n", time.Now().Format("2006-01-02 15:04:05"), fmt.Sprintf(format, args...))
+	fmt.Printf("[INFO] - %s - %s\n",
+		time.Now().Format("2006-01-02 15:04:05"), fmt.Sprintf(format, args...))
 }
 
 func (printfLogger) ErrorF(format string, args ...interface{}) {
-	fmt.Printf("[\033[31mERROR\033[0m] - %s - [\033[31m%s\033[0m]\n", time.Now().Format("2006-01-02 15:04:05"), fmt.Sprintf(format, args...))
+	fmt.Printf("[\033[31mERROR\033[0m] - %s - [\033[31m%s\033[0m]\n",
+		time.Now().Format("2006-01-02 15:04:05"), fmt.Sprintf(format, args...))
 }
